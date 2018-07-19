4ddbef5b5a65073d8b69cd6f241edf980fc27189
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2018-07-19 13:47:57 -0300

    menu-item component applied

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -7,6 +7,7 @@ import "rxjs/add/operator/catch"
 import { MEAT_API } from "../app.api"
 import { Observable } from "../../../node_modules/rxjs/Observable";
 import { ErrorHandler } from "../app.error-handler"
+import { MenuItem } from "../restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -27,6 +28,12 @@ export class RestaurantsService {
 
     reviewsOfRestaurant(id: string): Observable<any> {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
