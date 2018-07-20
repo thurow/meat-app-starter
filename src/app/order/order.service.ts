2f7666450134c52a0b36e9329b8938d2e224146c
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2018-07-20 18:41:49 -0300

    created order summary component, created route and navigating for this route when order is completed

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -42,5 +42,6 @@ export class OrderService {
                                 JSON.stringify(order),
                                 new RequestOptions({headers: headers}))
                         .map(response => response.json())
+                        .map(order => order.id)
     }
 }
