565e4f64bac6e03adf7b5396a3334955c0206fd0
Alexandre Thurow <alexandre@trezo.com.br>
2019-02-18 15:08:42 -0300

    order module, shared com providers ou não, preload

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,24 @@
-import {NgModule} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {InputComponent} from './input/input.component';
 import {RadioComponent} from './radio/radio.component';
 import {RatingComponent} from './rating/rating.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 
+import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import {OrderService} from '../order/order.service';
+import {RestaurantsService} from '../restaurants/restaurants.service';
+
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent],
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
   exports: [InputComponent, RadioComponent, RatingComponent, FormsModule, ReactiveFormsModule, CommonModule]
 })
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, OrderService, RestaurantsService]
+    }
+  }
+}
