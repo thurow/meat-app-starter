7b43080fe82a9244d072d1ec64795f481d00fe60
Alexandre Thurow <alexandre@trezo.com.br>
2019-02-22 13:09:51 -0300

    processo de login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,7 @@ import {OrderService} from '../order/order.service';
 import {RestaurantsService} from '../restaurants/restaurants.service';
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import { NotificationService } from './messages/notification.service';
+import {LoginService} from '../security/login/login.service';
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -20,7 +21,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [ShoppingCartService, OrderService, RestaurantsService, NotificationService]
+      providers: [ShoppingCartService, OrderService, RestaurantsService, NotificationService, LoginService]
     }
   }
 }
