dc8bbdbe45093efe3fd913539914b8453a913389
Alexandre Thurow <alexandre@trezo.com.br>
2019-02-22 13:23:36 -0300

    autorização para realizar compra no front

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -4,12 +4,15 @@ import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { Observable } from '../../../node_modules/rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Order, OrderItem } from './order.model';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { MEAT_API } from '../app.api';
+import {LoginService} from '../security/login/login.service';
 
 @Injectable()
 export class OrderService {
-    constructor(private cartService: ShoppingCartService, private http: HttpClient){}
+    constructor(private cartService: ShoppingCartService,
+                private http: HttpClient,
+                private loginService: LoginService) {}
 
     itemsValue(): number {
         return this.cartService.total()
@@ -36,7 +39,11 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        return this.http.post<Order>(`${MEAT_API}/orders`, order)
+        let headers = new HttpHeaders()
+        if (this.loginService.isLoggedIn()) {
+            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+        }
+        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers})
                         .map(order => order.id)
     }
 }
