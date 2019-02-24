693e7c779ce69705e05bf23145d57ed3aa696354
Alexandre Thurow <alexandre@trezo.com.br>
2019-02-24 15:55:24 -0300

    canActivate route

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -20,7 +20,7 @@ export const ROUTES: Routes = [
             {path: 'reviews', component: ReviewsComponent}
         ]},
     {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'order', loadChildren: './order/order.module#OrderModule', canLoad: [LoggedinGuard]},
+    {path: 'order', loadChildren: './order/order.module#OrderModule', canLoad: [LoggedinGuard], canActivate: [LoggedinGuard]},
     {path: 'order-summary', component: OrderSummaryComponent},
     {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: '**', component: NotFoundComponent}
--- src/app/security/loggedin.guard.ts
+++ src/app/security/loggedin.guard.ts
@@ -1,17 +1,25 @@
-import {CanLoad, Route} from '@angular/router';
+import {CanLoad, Route, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {LoginService} from './login/login.service';
 
 @Injectable()
-export class LoggedinGuard implements CanLoad {
+export class LoggedinGuard implements CanLoad, CanActivate {
 
     constructor(private loginService: LoginService) {}
 
-    canLoad(route: Route): boolean {
+    checkAuthentication(path: string): boolean {
         const loggedIn = this.loginService.isLoggedIn()
         if (!loggedIn) {
-            this.loginService.handleLogin(`/${route.path}`)
+            this.loginService.handleLogin(`/${path}`)
         }
         return loggedIn;
+    }
+
+    canLoad(route: Route): boolean {
+        return this.checkAuthentication(route.path)
+    }
+
+    canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean {
+        return this.checkAuthentication(activatedRoute.routeConfig.path)
     }
 }
